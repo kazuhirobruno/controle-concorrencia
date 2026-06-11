7f4ec126272dd451856858c024ad8ddde0df3aa1
unknown <brunokazuhiro@gmail.com>
2026-06-11 18:30:15 -0300

    feat: add swagger

--- src/main/java/br/com/example/kazuhiro/controle_transaction/modules/transaction/entities/Transaction.java
+++ src/main/java/br/com/example/kazuhiro/controle_transaction/modules/transaction/entities/Transaction.java
@@ -5,7 +5,8 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
-
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import br.com.example.kazuhiro.controle_transaction.modules.transaction.types.TransactionType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -23,13 +24,16 @@ public class Transaction {
   private UUID id;
 
   @NotBlank
+  @Schema(example = "c", description = "Indica o tipo de transação. Enviar apenas 'c' ou 'd' como valor.", requiredMode = RequiredMode.REQUIRED)
   private TransactionType tipo;
 
   @PositiveOrZero
+  @Schema(example = "1000", description = "Valor a ser debitado ou creditado.", requiredMode = RequiredMode.REQUIRED)
   private int valor;
 
   @NotBlank
   @Length(max = 250)
+  @Schema(example = "Valor a receber", description = "Descreve o motivo da transação.", requiredMode = RequiredMode.REQUIRED)
   private String descricao;
 
   @CreationTimestamp
--- src/main/java/br/com/example/kazuhiro/controle_transaction/modules/user/entitites/User.java
+++ src/main/java/br/com/example/kazuhiro/controle_transaction/modules/user/entitites/User.java
@@ -4,6 +4,9 @@ import java.util.UUID;
 
 import org.hibernate.validator.constraints.Length;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,15 +24,19 @@ public class User {
 
   @NotBlank
   @Length(max = 20)
+  @Schema(name = "username", description = "Nome de usuário para o login.", requiredMode = RequiredMode.REQUIRED)
   private String username;
 
   @NotBlank
   @Length(max = 20)
+  @Schema(name = "password", description = "Senha do usuário.", requiredMode = RequiredMode.REQUIRED)
   private String password;
 
   @PositiveOrZero
+  @Schema(name = "limite", description = "Limite do usuário.", requiredMode = RequiredMode.REQUIRED)
   private int limite;
 
+  @Schema(name = "saldo", description = "Saldo atual do usuário.", requiredMode = RequiredMode.REQUIRED)
   private int saldo;
 
 }
