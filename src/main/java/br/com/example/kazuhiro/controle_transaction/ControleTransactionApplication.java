de6c676cbd85d2bd84fe36d0e0c60759ed840150
unknown <brunokazuhiro@gmail.com>
2026-06-11 13:42:44 -0300

    feat: initial commit

---
+++ src/main/java/br/com/example/kazuhiro/controle_transaction/ControleTransactionApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.kazuhiro.controle_transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleTransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/example/kazuhiro/controle_transaction/ControleTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.kazuhiro.controle_transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
